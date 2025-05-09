58d3714f4a801c9fd98e9849872ee418de0e904c
skyhi-github <wama.skyhi@gmail.com>
2025-05-09 12:18:44 +0700

    new update

--- src/modules/redeem/redeem.module.ts
+++ src/modules/redeem/redeem.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedeemService } from './services/redeem.user.service';
+import { RedeemController } from './controllers/redeem.user.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [LeaveService],
-  controllers: [LeaveController],
+  imports: [TypeOrmModule.forFeature([])],
+  providers: [RedeemService],
+  controllers: [RedeemController],
 
 })
 export class RedeemModule {}
