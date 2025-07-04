49f6740fed5ac7fccc3d7dc8937e35fa09036c64
skyhi-github <skyhi>
2025-07-05 00:02:30 +0700

    chore: User module update

--- src/main.module.ts
+++ src/main.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
+import 'dotenv/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: Boolean(process.env.DB_SYNC),
+    }),
     ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public')
   }),
